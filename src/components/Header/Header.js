74f4f17d44bf68a8bd555f91beea1c0bb3b72977
dannyj172 <dannyj172@gmail.com>
2023-03-20 23:40:35 +0200

    Added Home, Parallax and started image showcase

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,15 +10,6 @@ export const Header = () => {
                 <h1>SNEAKER<span>THRIFT</span></h1>
             </div>
 
-            {/* <nav>
-                <Link to="#">Home</Link>
-                <Link to="#">Catalog</Link>
-                <Link to="#">Create Listing</Link>
-                <Link to="#">Logout</Link>
-                <Link to="#">Login</Link>
-                <Link to="#">Register</Link>
-            </nav> */}
-
             <ul className='header-menu'>
                 <Link to="#">Home</Link>
                 <Link to="#">Catalog</Link>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './Home.css';
+import showcase1 from "../../assets/showcase-1.jpg"
+import showcase2 from "../../assets/showcase-2.jpg"
+import showcase3 from "../../assets/showcase-3.jpg"
 
 export const Home = () => {
     return (
-        <div className='Home'>
+        <div className='home'>
 
-        </div>
+            <div className="home-introduction">
+                <div className="container spacing">
+                    <h1 className="primary-title">Amazing shoes at an amazing price</h1>
+                    <p>Number #1 shoe reseller globally. "SNEAKERTHRIFT" is here to provide you with the best footwear there is!</p>
+                    <Link to="#" className="btn home-button">See what we have</Link>
+                </div>
+            </div>
+
+            <div className='showcase'>
+                <img className='showcase-img' src={showcase1} height="400px" width="400px" alt='img' />
+                <img className='showcase-img' src={showcase2} height="400px" width="400px" alt='img' />
+                <img className='showcase-img' src={showcase3} height="400px" width="400px" alt='img' />
+            </div>
+        </div >
     )
 }
